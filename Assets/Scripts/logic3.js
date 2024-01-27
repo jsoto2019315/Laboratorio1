eb0e910e6ab3a5126ad1964f6ec15724d88e5cbe
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-01-26 20:02:32 -0600

    Implementacion de carrusel y modificaciones en el navbar superior

--- Assets/Scripts/logic3.js
+++ Assets/Scripts/logic3.js
@@ -14,3 +14,61 @@ listElements.forEach(listElement => {
         menu.style.height = `${height}px`
     })
 });
+
+
+function toggleMenu() {
+    var menu = document.getElementById("menuDesplegable");
+    menu.classList.toggle("mostrar");
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let slideIndex = 1; // Establece slideIndex en 1 para mostrar la primera imagen
+    showSlides(slideIndex);
+
+    // Función para cambiar manualmente los slides
+    window.plusSlides = function (n) {
+        showSlides(slideIndex += n);
+    };
+
+    function showSlides(n) {
+        let slides = document.getElementsByClassName("slide");
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex - 1].style.display = "block";
+    }
+});
+
+let currentIndex = 0;
+
+  function showSlide(index) {
+    const wrapper = document.getElementById('carousel-wrapper');
+    const items = document.querySelectorAll('.carousel-item');
+    const itemWidth = items[0].offsetWidth;
+
+    currentIndex = index;
+
+    const transformValue = -currentIndex * itemWidth + 'px';
+    wrapper.style.transform = 'translateX(' + transformValue + ')';
+  }
+
+  function prevSlide() {
+    currentIndex = (currentIndex - 1 + document.querySelectorAll('.carousel-item').length) % document.querySelectorAll('.carousel-item').length;
+    showSlide(currentIndex);
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % document.querySelectorAll('.carousel-item').length;
+    showSlide(currentIndex);
+  }
+
+  // Muestra la primera imagen al cargar la página
+  showSlide(currentIndex);
